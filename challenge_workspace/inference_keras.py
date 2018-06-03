eba0f349560e702abd68be1a9ce5f6d6437556da
n-log-n <faisal.nust@gmail.com>
2018-06-03 14:15:36 -0500

    Updated model file for inference

--- challenge_workspace/inference_keras.py
+++ challenge_workspace/inference_keras.py
@@ -13,10 +13,6 @@ file = sys.argv[-1]
 
 # Define encoder function
 def encode(array):
-	# pil_img = Image.fromarray(array)
-	# buff = BytesIO()
-	# pil_img.save(buff, format="PNG")
-	# return base64.b64encode(buff.getvalue()).decode("utf-8")
   retval, buffer = cv2.imencode('.png', array)
   return base64.b64encode(buffer).decode("utf-8")
 
@@ -27,13 +23,14 @@ answer_key = {}
 # Frame numbering starts at 1
 frame = 1
 
-MODEL_PATH = './saved_models/unet/unet_v8/model_saved.h5'
+# MODEL_PATH = './saved_models/fcn8/fcn8LowRes/fcn8VGG16LowRes.h5'
+MODEL_PATH = '/data/fcn8VGG16LowRes.h5'
 
 K.set_learning_phase(0)
 model = load_model(MODEL_PATH)
 #
-BATCH_SIZE=8
-IMG_SIZE=(416, 544)
+BATCH_SIZE=32
+IMG_SIZE=(384, 384)
 
 X_arr = np.zeros((BATCH_SIZE, IMG_SIZE[0], IMG_SIZE[1], 3), dtype=np.float32)
 m = video.shape[0]
@@ -43,7 +40,7 @@ binary_road_result = np.zeros((600, 800))
 for i in range(0, m, BATCH_SIZE):
   cnt = 0
   for j in range(i, min(i+BATCH_SIZE, m)):
-    X_arr[cnt] = cv2.resize(preprocess_input(video[j][184:, :, :].astype(np.float32)), (IMG_SIZE[1], IMG_SIZE[0]))
+    X_arr[cnt] = cv2.resize(preprocess_input(video[j].astype(np.float32)), (IMG_SIZE[1], IMG_SIZE[0]))
   
     cnt += 1
 
@@ -51,8 +48,8 @@ for i in range(0, m, BATCH_SIZE):
 
   for x in range(cnt):
     output = result[x].argmax(axis=2)
-    binary_car_result[184:, :] = cv2.resize((output == 0).astype(np.uint8), (800, 416))
-    binary_road_result[184:, :] = cv2.resize((output == 1).astype(np.uint8), (800, 416))
+    binary_car_result= cv2.resize((output == 0).astype(np.uint8), (800, 600))
+    binary_road_result = cv2.resize((output == 1).astype(np.uint8), (800, 600))
 
     answer_key[frame] = [encode(binary_car_result), encode(binary_road_result)]
     frame += 1
